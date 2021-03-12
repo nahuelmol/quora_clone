e6b44a90afa096c06775766f3da5c1b92f7ca955
Silvio <molinahuel44@gmail.com>
2021-03-12 01:30:29 -0300

    i should to fixe the login problem

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ app_name = 'core'
 
 urlpatterns = [
 	path('main/', views.main, name='main_url'),
+	path('login/',views.loginPage.as_view(),name='login'),
     path('home/',views.registerPage.as_view())
 	
 ]
--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from django.views import View
 from django.http import HttpResponse
 from django.views.generic.edit import FormView
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login, logout
 
 from .models import Users
 from .forms import users_form
@@ -22,22 +22,43 @@ class registerPage(View):
 
 	def post(self,request):
 		form = users_form(request.POST, request.FILES or None)
-		#
+		print(form)
 		if form.is_valid():
 			user = form.save(commit=False)
-			
-
 			user.password= make_password(form.cleaned_data.get('password'))
 			user.save()
-			print(user)
 
 			return redirect(reverse('core:main_url'))
 
 		content = {'form':form}
 		template = 'core/users_form.html'
 
-		return render(request, template, content)
+		return render(request,template,content)
 
 
+class loginPage(View):
 
+	def get(self,request):
+		form = users_form()
+		content = {'form':form}
+
+		template = 'core/login.html'
+
+		return render(request,template,content)
+	def post(self,request):
+		if request.method == 'POST':
+			name = request.POST.get('name')
+			password = request.POST.get('password')
+			password2 = make_password(password)
+			
+			user = authenticate(request,name=name,password=password2)
+			print(user)
+			if user is not None:
+				login(request,user)
+				return redirect('core:main_url')
+			else:
+				return HttpResponse('mal')
+		
+		context = {}
+		return render(request,'core/login.html',context)
 
